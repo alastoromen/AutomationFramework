a53691a83e4b643e80f7b08b42b7f8e6452add95
[omen] <[omen.369@icloud.com]>
2026-07-31 00:11:44 -0600

    se agrega estructura de carpetas

---
+++ src/main/java/com/medaina/App.java
@@ -0,0 +1,13 @@
+package com.medaina;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
